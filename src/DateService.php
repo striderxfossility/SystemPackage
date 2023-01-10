1a7f320d9f57256ec1976a50ed5cd18e5a046e0d
striderxfossility <y%^w7W%8Hc%t>
2023-01-10 15:04:48 +0100

    test

--- src/DateService.php
+++ src/DateService.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Jelle\Strider;
 
 use Carbon\Carbon;
---
+++ src/ExtraCollectServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+namespace Jelle\Strider;
+
+use Illuminate\Support\ServiceProvider;
+
+class ExtraCollectServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+}
