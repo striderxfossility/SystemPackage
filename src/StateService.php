47f89f9f6ac8305b0a55b0cf605021062400f2ce
striderxfossility <y%^w7W%8Hc%t>
2023-01-11 11:34:10 +0100

    added states

--- src/StateService.php
+++ src/StateService.php
@@ -4,6 +4,49 @@ namespace Jelle\Strider;
 class StateService {
     public static function offer($state)
     {
-        return $state;
+        switch ($state) {
+            case 'new':
+                return 'Nieuw';
+            
+            case 'sended':
+                return 'Verstuurd';
+            
+            case 'nagebeld':
+                return 'Nagebeld';
+            
+            case 'aanpassing':
+                return 'Aanpassing';
+            
+            case 'Afwachting':
+                return 'Afwachting';
+            
+            case 'Geweigerd':
+                return 'Geweigerd';
+            
+            case 'Akkoord':
+                return 'Akkoord';
+            
+            case 'Gefactureerd':
+                return 'Gefactureerd';
+            
+            case '0':
+                return 'Nieuw';
+            
+            case '1':
+                return 'Verzonden';
+            
+            case '2':
+                return 'Geannuleerd';
+            
+            case '3':
+                return 'Geaccepteerd';
+            
+            case '4':
+                return 'Gefactureerd';
+            
+            default:
+                return $state;
+        }
+        
     }
 }
