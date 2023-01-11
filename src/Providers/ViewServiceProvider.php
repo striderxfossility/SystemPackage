dcf4e8b6aeb26b53084111d029b9f96355d2ad3e
striderxfossility <y%^w7W%8Hc%t>
2023-01-11 10:13:49 +0100

    test

--- src/Providers/ViewServiceProvider.php
+++ src/Providers/ViewServiceProvider.php
@@ -7,8 +7,8 @@ class ViewServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__.'/resources/components/blocks',    'blocks');
-        $this->loadViewsFrom(__DIR__.'/resources/components/cols',      'cols');
+        $this->loadViewsFrom(__DIR__.'/../resources/components/blocks',    'blocks');
+        $this->loadViewsFrom(__DIR__.'/../resources/components/cols',      'cols');
         $this->loadViewsFrom(__DIR__.'/resources/components/forms',     'forms');
         $this->loadViewsFrom(__DIR__.'/resources/components/pages',     'pages');
         $this->loadViewsFrom(__DIR__.'/resources/components/table',     'table');
