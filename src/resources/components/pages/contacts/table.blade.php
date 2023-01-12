3196ae61be3114e8798b7da172d322a98e326f48
striderxfossility <y%^w7W%8Hc%t>
2023-01-12 10:06:22 +0100

    fix contact page table

--- src/resources/components/pages/contacts/table.blade.php
+++ src/resources/components/pages/contacts/table.blade.php
@@ -1,9 +1,11 @@
 <x-table-main>
     <x-table-head>
         <x-table-head-row>
-            <x-table-head-column>
-                project
-            </x-table-head-column>
+            @if(!class_exists("\App\Enums\ContactState"))
+                <x-table-head-column>
+                    project
+                </x-table-head-column>
+            @endif
             <x-table-head-column>
                 contact
             </x-table-head-column>
@@ -32,21 +34,46 @@
             @endif
 
             <x-table-body-row :link="$link">
-                <x-table-body-column>
-                    @if($contact->groothuis)
-                        {{ $contact->groothuis->project }} - {{ $contact->groothuis->omschrijving }}
-                    @endif
-                </x-table-body-column>
+                @if(!class_exists("\App\Enums\ContactState"))
+                    <x-table-body-column>
+                        @if($contact->groothuis)
+                            {{ $contact->groothuis->project }} - {{ $contact->groothuis->omschrijving }}
+                        @endif
+                    </x-table-body-column>
+                @endif
 
                 <x-table-body-column>
-                    @if ($contact != null)
-                        <div><i class="fa-solid fa-user pr-2"></i> {{ $contact->aanhef }} {{ $contact->achternaam }}</div>
-                    @endif  
-                    @if($contact->des == 1)
-                        <div><i class="fa-solid fa-building pr-2"></i> Groothuisbouw</div>
-                    @endif
-                    @if($contact->des == 2)
-                        <div><i class="fa-solid fa-building pr-2"></i> ABC Arkenbouw</div>
+                    @if(!class_exists("\App\Enums\ContactState"))
+                        <div class="text-blue-700">
+                            <i class="fa-solid fa-user pr-2"></i> 
+                                {{ $contact->aanhef }} {{ $contact->achternaam }}
+                        </div>
+
+                        @if($contact->des == 1)
+                            <div class="text-purple-700"><i class="fa-solid fa-building pr-2"></i> Groothuisbouw</div>
+                        @endif
+                        @if($contact->des == 2)
+                            <div class="text-purple-700"><i class="fa-solid fa-building pr-2"></i> ABC Arkenbouw</div>
+                        @endif
+                    @else
+                        <div class="text-blue-700">
+                            @if ($contact != null)
+                                @if($contact->state != \App\Enums\ContactState::Company->value)
+                                    <i class="fa-solid fa-user pr-2"></i> {{ $contact->first_name }} {{ $contact->last_name }}
+                                @endif
+                            @endif  
+                        </div>
+                        <div class="text-purple-700">
+                            @if ($contact != null)
+                                @if($contact->state == \App\Enums\ContactState::Company->value)
+                                    <i class="fa-solid fa-building pr-2"></i> {{ $contact->first_name }}
+                                @else
+                                    @if($contact->company != null)
+                                        <i class="fa-solid fa-building pr-2"></i> {{ $contact->company->first_name }}
+                                    @endif
+                                @endif
+                            @endif
+                        </div>
                     @endif
                 </x-table-body-column>
 
@@ -55,11 +82,16 @@
                 </x-table-body-column>
 
                 <x-table-body-column>
-                    {{ $contact->telefoon }}
+                    {{ $contact->telefoon }} {{ $contact->mobile }}
                 </x-table-body-column>
 
                 <x-table-body-column>
-                    {{ $contact->straat }}, {{ $contact->postcode }} te {{ $contact->plaats }}
+                    <div>{{ $contact->straat }}</div>
+                    <div>{{ $contact->postcode }}</div>
+                    <div>{{ $contact->plaats }}</div>
+                    <div>{{ $contact->adress }}</div>
+                    <div>{{ $contact->zipcode }}</div>
+                    <div>{{ $contact->city }}</div>
                 </x-table-body-column>
 
                 <x-table-body-column>
