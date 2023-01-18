54e35e72ae782c710472be3493ed5eea6dc41ee4
striderxfossility <y%^w7W%8Hc%t>
2023-01-18 09:58:56 +0100

    added add order teamleader

--- src/TeamleaderService.php
+++ src/TeamleaderService.php
@@ -22,14 +22,67 @@ class TeamleaderService {
 		$json_output = json_decode($api_output);
 		$output = $json_output?$json_output:$api_output;
 
-        dump($output);
-
 		return $output;
     }
 
-    public static function addOrderToTeamleader($order)
+    public static function add_order_to_teamleader($order)
     {
-        self::process('https://www.teamleader.be/api/orders.php', []);
-        dd($order);
+		$company_id = self::get_company('Badkamerstudio');
+		$ref = '';
+
+		if(isset($order->offer->contact))
+		{
+			if($order->offer->contact->groothuis != null)
+			{
+				$ref = $order->offer->contact->groothuis->project . ' - ' . $order->offer->contact->groothuis->omschrijving;
+			} else {
+				$ref = $order->offer->contact->last_name;
+			}
+		}
+
+		$title_offerte = 'Bestelbon - ' . $ref;
+
+
+		$fields = array(  
+			"contact_or_company"    => ("company"),
+			"contact_or_company_id" => ($company_id),
+			"title"                 => ($title_offerte),
+			"custom_field_41732"    => (date('d/m/Y', strtotime("+7 day"))),
+			"custom_field_61732"    => $order->straat . ' - ' . $order->postcode . ' - ' . $order->plaats,
+			"custom_field_132609"   => 299591,
+			"custom_field_132606"   => $order->delivery_date,
+			"source"                => "backend.badkamerstudio",
+			"subtitle_1"            => ("Referentie: " . $ref),
+			"vat_1"                 => (21),
+		);
+
+		if($order->orderproduct()->count() > 0)
+		{
+			for ($i=0; $i < $order->orderproduct()->count(); $i++) 
+			{ 
+				$tile = \App\Models\Tile::find($order->orderproduct()->get()[$i]->model_id);
+				$fields["description_" . $i + 1]  	= $order->orderproduct()->get()[$i]->number . ' - ' . $order->orderproduct()->get()[$i]->name; 
+				$fields["custom_descr_" . $i + 1] 	= ''; 
+				$fields["amount_" . $i + 1]       	= number_format($tile->squared_meters * $order->orderproduct()->get()[$i]->amount, 2);  
+				$fields["price_" . $i + 1] 			= $order->orderproduct()->get()[$i]->price;
+				$fields["product_unit_" . $i + 1]   = 3340;
+			}
+		}
+
+        self::process('https://www.teamleader.be/api/addDeal.php', $fields);
     }
+
+	public static function get_company($contact_email)
+	{
+		$fields = array(  
+			"searchby" => ($contact_email)
+		);
+
+		$contact = self::process("https://app.teamleader.eu/api/getCompanies.php?amount=1&pageno=0", $fields);
+
+		if($contact == '[]')
+			return null;
+		
+		return $contact[0]->id;
+	}
 }
