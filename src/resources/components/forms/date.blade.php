6b7bddc1669f8c692a33ee7cdc949814bd3252d4
striderxfossility <y%^w7W%8Hc%t>
2023-02-27 12:04:35 +0100

    added jquery

--- src/resources/components/forms/date.blade.php
+++ src/resources/components/forms/date.blade.php
@@ -4,6 +4,9 @@
 
 <input id="datepicker-{{ $name }}" class="datepicker-{{ $name }} block mt-1 w-full shadow-sm border-slate-300 focus:border-slate-300 focus:ring focus:ring-slate-200 focus:ring-opacity-50" type="text" id="{{ $name }}" name="{{ $name }}" value="{{ $value != '' ? $value : old($name) }}" />
 
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<script src="http://code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
+
 <script type="text/javascript"> 
     $( ".datepicker-{{ $name }}" ).datepicker({
         showWeek: true,
